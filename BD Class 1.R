4ff3822e9d9852cea91d2f6f11e5fccb8984f9b3
Mamatha-1122 <burramamatha4@gmail.com>
2024-04-06 23:30:11 -0400

    first commit

---
+++ BD Class 1.R
@@ -0,0 +1,62 @@
+#printing the program hello world
+"Hello world"
+#printing the assiging variables
+A<-5
+b<-10
+c<-a+b
+c
+#priniting the variables
+v1 <- 4.5
+v2 <- 2
+v3 <- v1/v2
+v3
+v4 <- sqrt(v1)
+v4
+greet <-"Hello,"
+name <-"Mamatha"
+message <-paste(greet,name)
+message
+#printing the data types
+x <-2L
+typeof(x)
+y <- 2.5
+typeof(y)
+z <- 2+3i
+typeof(z)
+a <-"h"
+typeof(a)
+q <- T
+typeof(q)
+p <- FALSE 
+typeof(p)
+#priniting the Basic Math
+a <- 5
+b <- 3
+c <- a+b
+d <- a-b
+e <- a * b
+f <- a/b
+
+a
+b
+c
+d
+e
+f
+# printing the Basic Logic
+4 <5
+10>100
+4==5
+
+result <-4 <5
+result
+typeof(result)
+
+result <-4 <5
+result
+typeof(result)
+result2 <-!(5>1)
+result2
+result | result2
+result & result2
+isTRUE(result)
