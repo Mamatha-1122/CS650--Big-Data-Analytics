2cafda16d2331a483eef83a38ba6cdbf14e2046d
Mamatha-1122 <burramamatha4@gmail.com>
2024-04-12 23:40:40 -0400

    first commit

---
+++ Assignment 1.R
@@ -0,0 +1,25 @@
+# let the numbers be x, y, and z
+x <- 7
+y <- 12
+z <- 5
+
+#doing the average calculation
+average <-(X +y + z) / 3
+print(paste("The average of",x, ","y, ",and",z, "is:", average))
+
+# Greeting message
+first_name <- "Mamatha"
+greet <- "Welcome to R,"
+message <- paste(greet, first_name)
+message
+
+#let x be 7, y be 12, and z be 4
+x <- 7
+y <- 12
+z <- 4
+#Print whether x is bigger than y
+print(x>y)
+
+#print whether x is bigger than z
+print(x > z)
+
